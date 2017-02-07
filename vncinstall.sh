b6e0f9b248541c74bc779585cdb29bea451f1448
David Cabinian <dhcabinian@gmail.com>
2017-02-07 15:47:30 -0500

    Commented VNC install

--- vncinstall.sh
+++ vncinstall.sh
@@ -1,3 +1,4 @@
+#https://medium.com/@joseison/running-ubuntu-desktop-gui-aws-ec2-instance-on-windows-3d4d070da434#.hkwfndh3r
 sudo apt-get -y update
 sudo apt-get -y install ubuntu-desktop tightvncserver gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
 vncserver :1
@@ -5,3 +6,4 @@ vncserver -kill :1
 sudo rm ~/.vnc/xstartup
 sudo cp xstartup ~/.vnc/xstartup
 vncserver :1
+#ssh -L 5901:127.0.0.1:5901 ubuntu@<IP Address> -i ~/.ssh/<ssh key>.pem
