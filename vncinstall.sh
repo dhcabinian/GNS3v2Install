1b78725ed0136b71e56f30503dfc0b4141739c6d
David Cabinian <dhcabinian@gmail.com>
2017-02-08 01:29:33 -0500

    Added virtualbox install script

--- vncinstall.sh
+++ vncinstall.sh
@@ -4,6 +4,12 @@ sudo apt-get -y install ubuntu-desktop tightvncserver gnome-panel gnome-settings
 vncserver :1
 vncserver -kill :1
 sudo rm ~/.vnc/xstartup
+sudo chmod 777 ~/.vnc 
 sudo cp xstartup ~/.vnc/xstartup
 vncserver :1
 #ssh -L 5901:127.0.0.1:5901 ubuntu@<IP Address> -i ~/.ssh/<ssh key>.pem
+
+# Will ask for VNC password
+# Password: mininet
+# Will ask if you want view only Password
+# No
